b43791ef141e1576dce04a56ca53a9e92bfa3bac
Joe <joseph.dung1@gmail.com>
2024-12-05 22:27:42 +0100

    Crud with Rust
    Testing Rust and postgres db connection using diesel

---
+++ src/schema.rs
@@ -0,0 +1,9 @@
+table! {
+    crud (id) {
+        id -> Int4,
+        fantasy_name -> Varchar,
+        real_name -> Nullable<Varchar>,
+        spotted_photo -> Text,
+        strength_level -> Int4,
+    }
+}
